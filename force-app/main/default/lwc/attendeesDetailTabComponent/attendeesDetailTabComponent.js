dc711cfae4f1fa9e5430b6eac07b9ebb25cb62b3
Kacper Konopka <kacper.konopka@cloobees.com>
2022-07-25 11:58:57 +0200

    added quick create button to event attendee/event speaker tab

--- force-app/main/default/lwc/attendeesDetailTabComponent/attendeesDetailTabComponent.js
+++ force-app/main/default/lwc/attendeesDetailTabComponent/attendeesDetailTabComponent.js
@@ -1,4 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 import getEventAttendees from '@salesforce/apex/AttendeeController.getEventAttendees';
 
 const columns = [
@@ -31,10 +33,24 @@ const columns = [
 
 ]
 
-export default class AttendeesDetailTabComponent extends LightningElement {
+export default class AttendeesDetailTabComponent extends NavigationMixin(LightningElement) {
     @api eventRecordId;
     columns = columns;
 
     @wire(getEventAttendees, {eventId: '$eventRecordId'})
     attendees;
+
+    handleClick(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+            objectApiName: 'Event_Attendee__c',
+            actionName: 'new'
+            },
+            state: {
+            defaultFieldValues: encodeDefaultFieldValues({Event__c: this.eventRecordId})
+            }
+        });
+         
+    }
 }
--- force-app/main/default/lwc/speakerDetailTabComponent/speakerDetailTabComponent.js
+++ force-app/main/default/lwc/speakerDetailTabComponent/speakerDetailTabComponent.js
@@ -1,12 +1,28 @@
 import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 import avatarLinkImport from '@salesforce/resourceUrl/default_picture';
 import getSpeakersRelatedWithEvent from '@salesforce/apex/SpeakerController.getSpeakersRelatedWithEvent';
 
-export default class SpeakerDetailTabComponent extends LightningElement {
+export default class SpeakerDetailTabComponent extends NavigationMixin(LightningElement) {
     @api eventRecordId;
 
     @wire(getSpeakersRelatedWithEvent, {eventId: '$eventRecordId'})
     speakers;
 
     avatarLink = avatarLinkImport;
+
+    handleClick(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+            objectApiName: 'EventSpeakers__c',
+            actionName: 'new'
+            },
+            state: {
+            defaultFieldValues: encodeDefaultFieldValues({Event__c: this.eventRecordId})
+            }
+        });
+         
+    }
 }
