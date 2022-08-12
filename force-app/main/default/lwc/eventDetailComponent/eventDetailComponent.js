feccddafd1eaa0fbe26f75f1e4498d09f41f7dbf
Kacper Konopka <kacper.konopka@cloobees.com>
2022-08-12 17:13:33 +0200

    hotfix do eventDetailComp

--- force-app/main/default/lwc/eventDetailComponent/eventDetailComponent.js
+++ force-app/main/default/lwc/eventDetailComponent/eventDetailComponent.js
@@ -1,5 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
+import { LightningElement, api} from 'lwc';
 
 export default class EventDetailComponent extends LightningElement {
     objectApiName = 'Event__c';
